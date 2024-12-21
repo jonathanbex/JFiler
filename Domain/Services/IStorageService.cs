1ac2218f3c7b1c743bb532840f0e3dc8dab867aa
JonathanBex <jonathan.soderberg@hotmail.com>
2024-12-21 11:58:31 +0100

    sync

--- Domain/Services/IStorageService.cs
+++ Domain/Services/IStorageService.cs
@@ -1,10 +1,10 @@
-﻿using JFiler.Domain.Models.ViewModel;
+﻿using JFiler.Domain.Models;
 
 namespace JFiler.Domain.Services
 {
   public interface IStorageService
   {
-    Task<FileResultViewModel> GetFilesAsync(string userDirectory, string? searchTerm, int page, int pageSize);
+    Task<FileResultModel> GetFilesAsync(string userDirectory, string? searchTerm, int page, int pageSize);
     Task UploadFileAsync(string userDirectory, IFormFile file, IProgress<double>? progress = null);
     Task DeleteFileAsync(string userDirectory, string fileName);
     Task<FileStream> GetFileStreamAsync(string? userDirectory, string fileName);

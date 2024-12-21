87d390d80ff8e50556dbef7a5901d79eedce2fec
JonathanBex <jonathan.soderberg@hotmail.com>
2024-12-21 14:17:29 +0100

    sync

--- Controllers/AuthenticationController.cs
+++ Controllers/AuthenticationController.cs
@@ -39,25 +39,11 @@ namespace JFiler.Controllers
       var authProperties = new AuthenticationProperties
       {
         AllowRefresh = true,
-        // Refreshing the authentication session should be allowed.
-
-        //ExpiresUtc = DateTimeOffset.UtcNow.AddMinutes(10),
-        // The time at which the authentication ticket expires. A 
-        // value set here overrides the ExpireTimeSpan option of 
-        // CookieAuthenticationOptions set with AddCookie.
 
         IsPersistent = true,
-        // Whether the authentication session is persisted across 
-        // multiple requests. When used with cookies, controls
-        // whether the cookie's lifetime is absolute (matching the
-        // lifetime of the authentication ticket) or session-based.
 
         IssuedUtc = DateTime.Now,
-        // The time at which the authentication ticket was issued.
 
-        //RedirectUri = <string>
-        // The full path or absolute URI to be used as an http 
-        // redirect response value.
       };
 
       await HttpContext.SignInAsync(CookieAuthenticationDefaults.AuthenticationScheme, new ClaimsPrincipal(claimsIdentity), authProperties);
